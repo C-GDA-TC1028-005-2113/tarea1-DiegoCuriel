300314af0abe705228e62c47358e78bdd84e4d97
DiegoCuriel <a01640372@itesm.mx>
2021-08-23 18:46:39 +0000

    Tarea1

--- assignments/03Promedio/src/exercise.py
+++ assignments/03Promedio/src/exercise.py
@@ -1,6 +1,18 @@
 def main():
     #escribe tu código abajo de esta línea
 
+    # Este programa calculará el promedio de 4 calificaciones
+    # Autor: Diego Curiel
+
+    n1 = int(input("Calificación de la materia: "))
+    n2 = int(input("Calificación de la materia: "))
+    n3 = int(input("Calificación de la materia: "))
+    n4 = int(input("Calificación de la materia: "))
+    resultado = str(float((n1+n2+n3+n4)/4))
+    print ("El promedio es:",resultado)
+
+
+
 
 if __name__ == '__main__':
     main()
